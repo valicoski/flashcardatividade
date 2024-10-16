fae0abb21a34d80f91b564b5a97ee9209416cc49
valicoski <165660723+valicoski@users.noreply.github.com>
2024-10-16 23:39:36 +0000

    alterações

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é programação?',
+    'O python é uma língua de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da frança é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é HI (RAI)'
+)
